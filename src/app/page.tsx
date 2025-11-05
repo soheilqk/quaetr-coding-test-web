cb092d583cff7fd6bee6d464d04276576f7ce171
Soheil Qorbani <soheil.q.k@gmail.com>
2025-11-05 19:34:27 +0100

    refactor: seo and serverside fetchin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,23 @@
-"use client";
+async function getCompanies() {
+  const res = await fetch("http://localhost:3000/api/companies", {
+    cache: "no-store",
+  });
 
-import { Inter } from "@next/font/google";
-import { useEffect, useState } from "react";
-const inter = Inter({ subsets: ["latin"] });
+  if (!res.ok) {
+    throw new Error("Failed to fetch companies");
+  }
 
-export default function Home() {
-  const [stuff1, setStuff1] = useState<any>([]);
-  useEffect(() => {
-    // declare the data fetching function
-    const fetchData = async () => {
-      const data = await fetch("/api/companies");
-      const data2 = await data.json();
-      console.log(data2.data);
-      setStuff1(data2.data);
-    };
+  return res.json();
+}
 
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
+export default async function Home() {
+  const { data: companies } = await getCompanies();
 
   return (
     <main>
-      <h2 className={inter.className}>Quartr</h2>
-      <p className={inter.className}>Trending companies</p>
-      <p>{JSON.stringify(stuff1)}</p>
+      <h2>Quartr</h2>
+      <p>Trending companies</p>
+      <p>{JSON.stringify(companies)}</p>
     </main>
   );
 }
